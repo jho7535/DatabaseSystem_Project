3ab234dc31eb6cd47e2688807165408f2496979a
jho7535 <jho7535@naver.com>
2024-12-09 19:14:23 +0900

    project init

---
+++ src/main/java/com/sw/club_management_system/ClubManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.sw.club_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClubManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClubManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sw/club_management_system/ClubManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sw.club_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
