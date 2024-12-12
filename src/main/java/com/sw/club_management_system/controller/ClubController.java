257ddc51b477960e264dc184def27d7d8406350c
jho7535 <jho7535@naver.com>
2024-12-12 16:18:56 +0900

    refactor : API 요청 url 분리

--- src/main/java/com/sw/club_management_system/controller/ClubController.java
+++ src/main/java/com/sw/club_management_system/controller/ClubController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clubs")
+@RequestMapping("/api/clubs")
 @RequiredArgsConstructor
 public class ClubController {
 
--- src/main/java/com/sw/club_management_system/controller/DocumentController.java
+++ src/main/java/com/sw/club_management_system/controller/DocumentController.java
@@ -12,7 +12,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @RestController
-@RequestMapping("/documents")
+@RequestMapping("/api/documents")
 @RequiredArgsConstructor
 public class DocumentController {
 
